3b135dfbec2ee2b347da0115b2e0ae3349f6adae
fbk-rena <fbk.rena@gmail.com>
2017-06-08 12:16:18 -0500

    nab-side no funciona, tarjetas agregadas

--- js/mapa.js
+++ js/mapa.js
@@ -1,31 +1,34 @@
 function cargarPagina() {
-  obtenerUbicacion();
-  $("#map").click(cambiarUbicacion);
+    $(".btn-perfil").sideNav();
+    obtenerUbicacion();
+    $("#map").click(cambiarUbicacion);
+   
+
 }
 
 var obtenerUbicacion = function (e) {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(mostrarPosicion);
-	} else {
-		alert("Actualice su navegador");
-	}
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrarPosicion);
+    } else {
+        alert("Actualice su navegador");
+    }
 };
 
 var mostrarPosicion = function (posicion) {
-	var coordenadas = {
-		lat: posicion.coords.latitude,
-		lng: posicion.coords.longitude
-	};
-	mostrarMapa(coordenadas);
+    var coordenadas = {
+        lat: posicion.coords.latitude,
+        lng: posicion.coords.longitude
+    };
+    mostrarMapa(coordenadas);
 };
 
 var mostrarMapa = function (coordenadas) {
-	var map = new google.maps.Map($('#map')[0], {
-      zoom: 17,
-      center: coordenadas
+    var map = new google.maps.Map($('#map')[0], {
+        zoom: 17,
+        center: coordenadas
     });
     var marker = new google.maps.Marker({
-      position: coordenadas,
-      map: map
+        position: coordenadas,
+        map: map
     });
 }
