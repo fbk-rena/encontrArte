a2b2643372cbe21b9c71be1345fac201abe389ef
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-06-08 13:55:06 -0500

    splash y parte de la version desktop

--- js/pagInicio.js
+++ js/pagInicio.js
@@ -3,5 +3,23 @@ $(document).ready(function(){
       $('#splash').hide(500);
       $('#home').show(500);
    }, 2000);
+   $('.slider').slider();
 
 });
+
+
+$( window ).scroll(navegador);
+
+
+function navegador(){
+
+    var $nav = $('.nav-wrapper');
+    var scroll = document.body.scrollTop;
+    if(scroll >= 100){
+      $nav.css('background-color', 'rgba(71, 45, 103, 0.74)');
+      $('nav').css('background-color', 'rgba(71, 45, 103, 0.08)');
+  } else if (scroll <= 483){
+      $nav.css('background-color', '#47334d');
+      $('nav').css('background-color', '#47334d');
+  }
+}
